dc9a7aa9b6a889b723f7023c7a9b53ee799d14d7
ducpm3009 <hklbndvl@gmail.com>
2017-03-20 19:14:36 +0700

    Ducpm

---
+++ AndroidFive/app/src/main/java/com/example/ducpm/LayoutContainerActivity.java
@@ -0,0 +1,15 @@
+package com.example.ducpm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.ducpm.androidfive.R;
+
+public class LayoutContainerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_container);
+    }
+}
